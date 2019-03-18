22bb8f65962bdbc03b7a42062a31c24793b6147c
luoxiaobu <luoxiaobu@users.noreply.github.com>
2019-03-18 23:56:43 +0800

    add html-webpack-plugin

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 let str = require('./a.js')
 console.log('str')
+
+var s = ()=>{
+  console.log('123')
+}
+
+s()
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+let htmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path');
 
 module.exports= {
@@ -6,7 +7,36 @@ module.exports= {
     app: './src/index.js'
   },
   output: {
-    filename: '[name].js',
+    filename: '[name].[hash:5].js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  devServer: { // 开发服务器配置
+    port:3000,
+    progress: true,
+    contentBase: './dist',
+    // disableHostCheck: true
+  },
+  module: {
+    rules:[{
+      test: /\.js$/,
+      use: [{
+        loader: 'babel-loader',
+        options: {
+          presets: [
+          '@babel/preset-env'
+          ]
+        }
+      }]
+    }]
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      template: './index.html',
+      filename: 'index.html',
+      minify: {
+        collapseWhitespace: true,
+      },
+     // hash: true 给引入的文件加上hash
+    })
+  ]
 }
